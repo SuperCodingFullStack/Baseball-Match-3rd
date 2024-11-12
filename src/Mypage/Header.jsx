98f71801cdfce0ff7ee8a8e2337ee472ef3524c0
otter1614 <als2ek1614@gmail.com>
2024-11-12 19:11:05 +0900

    안녕

---
+++ src/Mypage/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>이미지</div>
+      <div>
+        <div>설정</div>
+        <div>알림</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Mypage/Mypage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+
+const Mypage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Mypage;

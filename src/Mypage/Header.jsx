d65728954145cb1c9a172e8b28cdac3e1a8276fe
otter1614 <als2ek1614@gmail.com>
2024-11-13 12:38:21 +0900

    기능 구현전 병합

--- src/Mypage/Header.jsx
+++ src/Mypage/Header.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import { GrMailOption } from "react-icons/gr";
+import { GrSettingsOption } from "react-icons/gr";
+
+const Div_LogoImg = styled.div`
+  width: 100px;
+  height: 70px;
+`;
+const Div_Containner = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Header = () => {
   return (
-    <div>
-      <div>이미지</div>
-      <div>
-        <div>설정</div>
-        <div>알림</div>
-      </div>
-    </div>
+    <Div_Containner>
+      <Div_LogoImg>로고 이미지</Div_LogoImg>
+      <Div_Containner>
+        <GrSettingsOption size="70px" className="GrSettingsOption" />
+        <GrMailOption size="70px" />
+      </Div_Containner>
+    </Div_Containner>
   );
 };
 

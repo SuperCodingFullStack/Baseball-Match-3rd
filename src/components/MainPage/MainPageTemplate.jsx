f74b70fdb5a75383f565fddf8c8abefb8d848b4e
hhaemin <ahrgoals@naver.com>
2024-11-13 11:01:01 +0900

    feat. mainpage 제작 template

---
+++ src/components/MainPage/MainPageTemplate.jsx
@@ -0,0 +1,13 @@
+import SwiperComponent from "../Body/Swiper/SwiperComponent";
+import Header from "./Header/Header";
+
+const MainPageTemplate = () => {
+  return (
+    <>
+      <Header />
+      <SwiperComponent />
+    </>
+  );
+};
+
+export default MainPageTemplate;

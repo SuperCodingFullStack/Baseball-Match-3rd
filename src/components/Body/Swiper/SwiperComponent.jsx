94b1561a1e03252c15bd9968ba03f7290c653378
hhaemin <ahrgoals@naver.com>
2024-11-13 11:05:37 +0900

    feat. mainpage-body swiper

---
+++ src/components/Body/Swiper/SwiperComponent.jsx
@@ -0,0 +1,32 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+export default function SwiperComponent() {
+  return (
+    <>
+      <Swiper
+        loop={true}
+        cssMode={true}
+        navigation={true}
+        pagination={{ clickable: true }}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mainSwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}

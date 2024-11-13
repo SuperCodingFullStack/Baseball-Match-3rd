434c9b3873e08650e074787d1454589547d1adda
otter1614 <als2ek1614@gmail.com>
2024-11-13 12:46:56 +0900

    Revert "기능 구현전 병합"
    This reverts commit d65728954145cb1c9a172e8b28cdac3e1a8276fe.

--- src/Mypage/Mypage.jsx
+++ src/Mypage/Mypage.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import Header from "./Header";
 
-const Wrapper = styled.div`
-  width: 1336px;
-`;
-
 const Mypage = () => {
   return (
-    <Wrapper>
+    <div>
       <Header />
-    </Wrapper>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-import Mypage from "./Mypage/Mypage";
+import Mypage from "./Mypage/Mypage.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

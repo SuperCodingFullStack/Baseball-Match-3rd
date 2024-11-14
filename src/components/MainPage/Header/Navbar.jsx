309143eccc295bddfe27b2af3db095c19a63a62c
hhaemin <ahrgoals@naver.com>
2024-11-14 11:32:29 +0900

    헤더 네비게이션바 수정

--- src/components/MainPage/Header/Navbar.jsx
+++ src/components/MainPage/Header/Navbar.jsx
@@ -1,108 +1,143 @@
 import { useState } from "react";
-import styled from "styled-components";
-import { FaChevronDown } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import DropdownTeam from "./DropdownTeam";
+import DropdownPost from "./DropdownPost";
+import DropdownFriends from "./DropdownFriends";
 
 const Navbar = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState({
-    team: false,
-    post: false,
-    friend: false,
-  });
+  const [activeDropdown, setActiveDropdown] = useState(null);
 
-  const handleDropdownToggle = (menu) => {
-    // 이전 상태를 기반으로 새로운 상태 설정
-    setDropdownOpen((prev) => ({
-      ...prev, // 이전 상태 유지
-      [menu]: !prev[menu], // 속성의 값 반전
-    }));
+  const handleMouseEnter = (dropdown) => {
+    setActiveDropdown(dropdown);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveDropdown(null);
   };
 
   return (
-    <NavbarContainer>
-      <NavItem onClick={() => handleDropdownToggle("team")}>
-        구단 관리 <FaChevronDown />
-        {isDropdownOpen.team && (
-          <DropdownMenu isOpen={isDropdownOpen}>
-            <DropdownItem>롯데 자이언츠</DropdownItem>
-            <DropdownItem>두산 베어스</DropdownItem>
-            <DropdownItem>LG Twins</DropdownItem>
-            <DropdownItem>KT wiz</DropdownItem>
-            <DropdownItem>한화 이글스</DropdownItem>
-            <DropdownItem>NC Dinos</DropdownItem>
-            <DropdownItem>기아 타이거즈</DropdownItem>
-            <DropdownItem>키움 히어로즈</DropdownItem>
-            <DropdownItem>삼성 라이온즈</DropdownItem>
-            <DropdownItem>SSG Landers</DropdownItem>
-          </DropdownMenu>
-        )}
-      </NavItem>
-      <NavItem onClick={() => handleDropdownToggle("post")}>
-        게시글 관리 <FaChevronDown />
-        {isDropdownOpen.post && (
-          <DropdownMenu isOpen={isDropdownOpen}>
-            <DropdownItem>게시판</DropdownItem>
-            <DropdownItem>내가 작성한 게시글</DropdownItem>
-          </DropdownMenu>
-        )}
-      </NavItem>
-      <NavItem onClick={() => handleDropdownToggle("friend")}>
-        친구 <FaChevronDown />
-        {isDropdownOpen.friend && (
-          <DropdownMenu isOpen={isDropdownOpen}>
-            <DropdownItem>나의 친구목록</DropdownItem>
-          </DropdownMenu>
-        )}
-      </NavItem>
-      <NavLink to="/mypage">마이 페이지</NavLink>
-    </NavbarContainer>
+    <>
+      <NavbarContainer>
+        <NavItem
+          onMouseOver={() => handleMouseEnter("teamInfo")}
+          onMouseOut={handleMouseLeave}
+        >
+          구단 정보
+        </NavItem>
+
+        <NavItem
+          onMouseOver={() => handleMouseEnter("posts")}
+          onMouseOut={handleMouseLeave}
+        >
+          게시글
+        </NavItem>
+
+        <NavItem
+          onMouseOver={() => handleMouseEnter("friends")}
+          onMouseOut={handleMouseLeave}
+        >
+          친구
+        </NavItem>
+        {/* <NavLink to="/mypage">마이 페이지</NavLink> */}
+      </NavbarContainer>
+
+      {activeDropdown === "teamInfo" && (
+        <DropdownMenu
+          onMouseOver={() => handleMouseEnter("teamInfo")}
+          onMouseOut={handleMouseLeave}
+        >
+          <DropdownItem>
+            <DropdownTeam />
+          </DropdownItem>
+        </DropdownMenu>
+      )}
+
+      {activeDropdown === "posts" && (
+        <DropdownMenu
+          onMouseOver={() => handleMouseEnter("posts")}
+          onMouseOut={handleMouseLeave}
+        >
+          <DropdownItem>
+            <DropdownPost />
+          </DropdownItem>
+        </DropdownMenu>
+      )}
+
+      {activeDropdown === "friends" && (
+        <DropdownMenu
+          onMouseOver={() => handleMouseEnter("friends")}
+          onMouseOut={handleMouseLeave}
+        >
+          <DropdownItem>
+            <DropdownFriends />
+          </DropdownItem>
+        </DropdownMenu>
+      )}
+    </>
   );
 };
 
 const NavbarContainer = styled.nav`
   display: flex;
-  padding: 1rem;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 const NavItem = styled.div`
+  position: relative;
   cursor: pointer;
-  margin-right: 4rem;
-  display: inline-block;
-  padding: 10px;
+  font-weight: 500;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  width: 9rem;
+  height: 5.5rem;
   &:hover {
     color: #799d40;
+    border-bottom: 5px solid #799d40;
+    transition: border-bottom 0.3s ease;
+  }
+`;
+
+const dropdownAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
   }
 `;
 
 const DropdownMenu = styled.div`
-  background-color: white;
+  z-index: 5;
   position: absolute;
-  margin-top: 1.7rem;
-  justify-content: center;
+  top: 100%;
+  width: 100%;
+  animation: ${dropdownAnimation} 0.8s ease forwards;
 `;
 
 const DropdownItem = styled.div`
   color: black;
-  font-size: 0.9rem;
-  font-weight: 400;
+  font-size: 1rem;
+  font-weight: 500;
   text-align: center;
   padding: 0.4rem 0.2rem;
-  &:hover {
-    background-color: #dbdbdb;
-    color: #799d40;
-  }
 `;
 
-const NavLink = styled(Link)`
-  display: inline-block;
-  text-align: center;
-  font-weight: 400;
-  color: black;
-  padding: 10px;
-  border-radius: 0.2rem;
-  &:hover {
-    color: #799d40;
-  }
-`;
+// const NavLink = styled(Link)`
+//   display: inline-block;
+//   text-align: center;
+//   font-weight: 400;
+//   color: black;
+//   padding: 10px;
+//   border-radius: 0.2rem;
+//   &:hover {
+//     color: #799d40;
+//   }
+// `;
 
 export default Navbar;

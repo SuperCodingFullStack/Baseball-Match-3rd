478c94d2fd45b18fae15d7a7be02c524cd8638c1
hhaemin <ahrgoals@naver.com>
2024-11-14 11:31:22 +0900

    헤더 구조 수정

--- src/components/MainPage/Header/Header.jsx
+++ src/components/MainPage/Header/Header.jsx
@@ -1,33 +1,42 @@
 import styled from "styled-components";
 import Navbar from "./Navbar";
-import { CiBaseball } from "react-icons/ci";
+import { FaBaseballBatBall } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import Icon from "./icon";
 
 const Header = () => {
   return (
     <Container>
-      <Link to="/">
-        <Logo>
-          <CiBaseball />
-        </Logo>
-      </Link>
-      <Navbar />
+      <LeftSection>
+        <Link to="/">
+          <Logo>
+            <FaBaseballBatBall />
+            Ball Mate
+          </Logo>
+        </Link>
+        <Navbar />
+      </LeftSection>
       <Icon />
     </Container>
   );
 };
 
 const Container = styled.div`
-  z-index: 100;
+  z-index: 10;
   position: fixed;
   top: 0;
   width: 100%;
   background-color: white;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid #dbdbdb;
+`;
+
+const LeftSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
 `;
 
 const Logo = styled.div`
@@ -36,6 +45,7 @@ const Logo = styled.div`
   font-size: 3rem;
   cursor: pointer;
   color: #799d40;
+  align-items: center;
 `;
 
 export default Header;

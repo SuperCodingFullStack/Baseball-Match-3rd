35ab4baa5831385da5b87e580c1451eb3e629720
rukadeveloper <cotasi135@gmail.com>
2024-11-19 16:06:12 +0900

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PartyPostLists from "./pages/partyPost/PartyPostLists";
 import PartyPost from "./pages/partyPost/PartyPost";
 import PartyPostWrite from "./pages/partyPost/PartyPostWrite";
 import { Reset } from "styled-reset";
-import Mypage from "./Mypage/Mypage";
+import Mypage from "./components/Mypage/Mypage";
 
 function App() {
   return (

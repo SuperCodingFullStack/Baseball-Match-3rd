a1c02785071a284db0f0ca38d7812d0d8453a16e
otter1614 <als2ek1614@gmail.com>
2024-11-19 15:57:47 +0900

    병합완

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "./Header/Header";
+import Header from "../components/Mypage/Header/Header";
 
 const Wrapper = styled.div`
   width: 1336px;

dd0f7ecec91b8ca479543099f02a2646d09c5ee0
hhaemin <ahrgoals@naver.com>
2024-11-20 11:29:52 +0900

    style 수정

--- src/components/MainPage/Header/DropdownPost.jsx
+++ src/components/MainPage/Header/DropdownPost.jsx
@@ -3,16 +3,21 @@ import { GoPencil } from "react-icons/go";
 import { IoHeartOutline } from "react-icons/io5";
 import { IoClipboardOutline } from "react-icons/io5";
 import { LuClipboardEdit } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 const DropdownPost = () => {
   return (
     <PostContainer>
-      <WritePost>
-        <StyledIcon as={GoPencil} />글 작성하기
-      </WritePost>
-      <Board>
-        <StyledIcon as={IoClipboardOutline} /> 게시판
-      </Board>
+      <Link to="/postWrite">
+        <WritePost>
+          <StyledIcon as={GoPencil} />글 작성하기
+        </WritePost>
+      </Link>
+      <Link to="/partyPosts">
+        <Board>
+          <StyledIcon as={IoClipboardOutline} /> 게시판
+        </Board>
+      </Link>
       <MyPost>
         <StyledIcon as={LuClipboardEdit} /> 내가 쓴 게시물
       </MyPost>

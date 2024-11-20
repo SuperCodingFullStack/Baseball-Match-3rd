2d0bd78f9d002932e79c33c184b1f80552e45178
hhaemin <ahrgoals@naver.com>
2024-11-20 11:29:35 +0900

    작업 진행중

--- src/components/MainPage/Body/WeeklyCalendar.jsx
+++ src/components/MainPage/Body/WeeklyCalendar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import dayjs from "dayjs";
 import { useState } from "react";
+import { LOTTE, NC } from "../../../constants";
 
 const WeeklyCalendar = () => {
   const [currentWeek, setCurrentWeek] = useState(dayjs().startOf("week"));
@@ -13,12 +14,19 @@ const WeeklyCalendar = () => {
     setCurrentWeek(currentWeek.add(1, "week"));
   };
 
+  const handleToday = () => {
+    setCurrentWeek(dayjs().startOf("week"));
+  };
+
   const weekDays = Array(7)
     .fill(0)
     .map((_, index) => currentWeek.add(index, "day"));
 
+  const today = dayjs().startOf("day");
+
   return (
     <CalendarContainer>
+      <TodayButton onClick={handleToday}>Today</TodayButton>
       <Header>
         <button onClick={handlePrevWeek}>{"<"}</button>
         <span>
@@ -29,15 +37,29 @@ const WeeklyCalendar = () => {
       </Header>
       <Days>
         {weekDays.map((day) => (
-          <Day key={day.format("YYYY-MM-DD")}>
+          <Day
+            key={day.format("YYYY-MM-DD")}
+            className={day.isSame(today, "day") ? "today" : ""}
+            isSaturday={day.day() === 6}
+            isSunday={day.day() === 0}
+          >
             <div>{day.format("ddd")}</div>
             <div>{day.format("D")}</div>
           </Day>
         ))}
       </Days>
       <Content>
-        <h3>날짜별 내용</h3>
-        <p></p>
+        <Game1>
+          <div>
+            <img src={LOTTE} />
+            <p>롯데</p>
+            <div>
+              <span>VS</span>
+            </div>
+            <img src={NC} />
+            <p>NC</p>
+          </div>
+        </Game1>
       </Content>
     </CalendarContainer>
   );
@@ -47,7 +69,7 @@ const CalendarContainer = styled.div`
   width: 100%;
   max-width: 1000px;
   margin: auto;
-  margin-top: 2rem;
+  margin-top: 3rem;
   text-align: center;
   padding: 1rem;
   border: 1px solid #ddd;
@@ -57,7 +79,7 @@ const CalendarContainer = styled.div`
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 1rem;
+  margin-top: 2rem;
   margin-bottom: 1rem;
   button {
     background: #ddd;
@@ -74,30 +96,65 @@ const Header = styled.div`
   }
 `;
 
+const TodayButton = styled.button`
+  display: flex;
+  justify-self: end;
+  background: #acfe49;
+  color: black;
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  &:hover {
+    color: white;
+    border: none;
+  }
+`;
+
 const Days = styled.div`
   display: flex;
   justify-content: space-around;
-  margin-bottom: 1rem;
+  margin-bottom: 2rem;
 `;
 
 const Day = styled.div`
-  display: flex;
+  display: grid;
   flex-direction: column;
   align-items: center;
   font-size: 1rem;
-  div:first-child {
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  box-sizing: border-box;
+  border-radius: 8px;
+  &:hover {
+    background-color: #f5f5f5;
+  }
+
+  div {
     font-weight: bold;
+    color: ${(props) =>
+      props.isSaturday ? "blue" : props.isSunday ? "red" : "inherit"};
+  }
+
+  &.today {
+    border: 1px solid #acfe49;
+    border-radius: 8px;
+    padding: 0.1rem;
   }
 `;
 
-const Content = styled.div`
-  margin-top: 1rem;
-  text-align: left;
-  h3 {
-    margin-bottom: 0.5rem;
-  }
-  p {
-    font-size: 1rem;
+const Content = styled.ul`
+  margin-top: 3rem;
+  text-align: center;
+`;
+
+const Game1 = styled.li`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  img {
+    weight: 40px;
+    height: 40px;
   }
 `;
 

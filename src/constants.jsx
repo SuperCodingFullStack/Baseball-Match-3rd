5b0f54c365c73e2e426aa0d9598a138be81bda1e
hhaemin <ahrgoals@naver.com>
2024-11-21 15:45:51 +0900

    img 저장방식 변경

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,29 +1,19 @@
-export const LOTTE =
-  "https://i.namu.wiki/i/FmfrbzPytldYTnvPi5m6s-gEAZWv1G4uBMxqnMYtzFDU_vGLQf4nSVhtoEHUs3F7tiSqjCLNZBN4Go7fMt-g3Zl7lN4CUqGDLqvrXJMhSDx3Bg2ShnJgTI5LtfDUjCe1kNWHHACBhHLdMuHLTPajJg.svg";
+export const LOTTE = "/assets/lotte.svg";
 
-export const KIA =
-  "https://i.namu.wiki/i/IeiOCgKsHfF-mNDUCjm8i-8qrxGqEf8Uey9aoQ-MBtRfau7ejaYyXn1lzPQVrOB_9TnJx8-ho4AHDmYW7JoMXGiBCeDgyTk0EcSUfMuV-EMiQTmv2BvdnMvCw6WFptrnU4VwSfRqRw5HpjKtIG2nYg.svg";
+export const KIA = "/assets/kia.svg";
 
-export const SAMSUNG =
-  "https://i.namu.wiki/i/rMpSXR7sFZyDuWoY0KarBWfbkiIwNRIFZDgHs-DMF4bmAu7_l_lDIMZ7Kym0I4tXPFxaPO8TIQW0iqa_NIRdUCAwPkCz6qB5uMjmSksFlpihEwbWx_w3-k89iVrGtZHaOjh1LHFOtd3zQCnzgCnPhg.svg";
+export const SAMSUNG = "/assets/samsung.svg";
 
-export const KT =
-  "https://i.namu.wiki/i/zH1I_eQtDPMEchRJxTmbCHCyILnShC5_3Ch7Sm-Fz7gYPUuGmXnovHU8ky2Ja08pkulfbUn7vyJ5FDqaWpHsrg.svg";
+export const KT = "/assets/kt.svg";
 
-export const HANHWA =
-  "https://i.namu.wiki/i/FfE2O-hEbg4_rIOOQ5m9UfA8JfZ2CJ5CKUypFCZ9WbtuSm0_rxzB59CobcvR95KlZBP_TkgvXnWXF1GlAmRcI3DXxsvpb9qPE8bG5lxKUErvUSA3Y0hUoZMB9EGLG14HwhE2fIlJWR5ZdN4jnruJNw.webp";
+export const HANWHA = "/assets/hanwha.svg";
 
-export const KIWOOM =
-  "https://i.namu.wiki/i/1W0wUxLcpIhn3qnk1oL1CKRRHVmvpsqWPaVPrMfqz3xlHoaf0PskfTdWrPX3bG5Vtolx2-uUqkOQulLIeAhFdPdW8fq-k3Oq4IZWUoqf6BDJjzuu3SqD1g3kFOahRsuXl9DQeJ3GIod8nzdgHt4Ung.svg";
+export const KIWOOM = "/assets/kiwoom.svg";
 
-export const LG =
-  "https://i.namu.wiki/i/kFcjn8hfYSdBLnK2_nh_WETHhm5tudy6lT0Y7V9ewBoUuaO8wEIeXpfUHUlpyf_8w0zCDZR7KQxRlfDpgAzYSr0B0j4aPhkQWXXpupRjlfiXT8P1-HJRTWv3mCTRXvpVF5cbRCX70wwXxekVVzaNIg.svg";
+export const LG = "/assets/lg.svg";
 
-export const NC =
-  "https://i.namu.wiki/i/1MT0u8IY04bX68HjVNohwMc6wo4DlsQF6mD2YlzuD1q3LYJYjQotNNAeK0PeSI1ronSTlIgwWvOqJKXGelswE6kQGuNyY-c90HNh5LlhmKA-SS0511ta_rSUU2yf4H0XnF4bKKuxq6slFPwhw_ZHAg.svg";
+export const NC = "/assets/nc.svg";
 
-export const DOOSAN =
-  "https://i.namu.wiki/i/eXJ8bTw7fjHHjdqk52HA5E2QXvV88Zj6QFbqW6Q4rCwktITn--8I8drqYVYrA0mG_uTjo8Nyle8t7qTxAw02iTPujwSG40_N8m26loOMlYL_gm_L8XsP2f_WgREMPorwtzB6PLwTUS4Qn2470ySLVw.svg";
+export const DOOSAN = "/assets/doosan.svg";
 
-export const SSG =
-  "https://i.namu.wiki/i/mm8_Ei8Nkqhmv0rdYr0n344GQtI-f0wcKI2uj7hknQxfquVN47FMQmUDh0hLmVOPrJlnjxOWUSYVMNOg62UXnYERSO6AGhbBQ5_P11vkuo-wS24BmHgfN7RgA-dyakM6nyXmvhcp-9EjwvmhRIZeyg.svg";
+export const SSG = "/assets/ssg.svg";

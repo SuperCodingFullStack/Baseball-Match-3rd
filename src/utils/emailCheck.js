26ed8f18c8bd04ff16205aa58a9e9bb1141b500f
rukadeveloper <cotasi135@gmail.com>
2024-11-25 21:11:25 +0900

    곧리펙터링할거

--- src/utils/emailCheck.js
+++ src/utils/emailCheck.js
@@ -1,5 +1,16 @@
-export const emailCheck = (email) => {
+import axios from "axios";
+
+export const emailCheck = async (email) => {
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/api/user/email?email=${email}`
+    );
+    const data = response.data;
+  } catch (error) {
+    console.error(error);
+  }
 
   if (regex.test(email)) {
     return {

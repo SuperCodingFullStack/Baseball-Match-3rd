cafa6b0efce04e3ef17f4abadd931c3d93e08f2e
hhaemin <ahrgoals@naver.com>
2024-11-27 15:26:46 +0900

    수정사항 없음

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import partyPostReducer from "./slice/partyPostSlice";
-import activeIdSlice from "./Slice/ActiveIdSlice";
+import activeIdSlice from "./Slice/activeIdSlice";
 import signUpSlice from "./slice/signUpSlice";
 import signUpErrorSlice from "./slice/signUpErrorSlice";
 import signUpNestSlice from "./slice/signUpNestSlice";

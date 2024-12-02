1493be9bcad11cd179e6452c1a1cc5805fdbc51c
hhaemin <ahrgoals@naver.com>
2024-12-02 17:28:28 +0900

    chore.친구목록 백엔드 연동코드 작성

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -3,17 +3,17 @@ import Header from "../components/MainPage/Header/Header";
 import styled from 'styled-components';
 
 const Friends = () => {
-    const [friends, setFriends] = useState([]); // 친구 목록 데이터 상태
+    // const [friends, setFriends] = useState([]); // 친구 목록 데이터 상태
 
-    useEffect(() => {
-        axios.get("http://localhost:8080/api/friend")
-            .then((response) => {
-                setFriends(response.data);
-            })
-            .catch((error) => {
-                console.error('데이터 불러오기 실패:', error);
-            });
-    }, []);
+    // useEffect(() => {
+    //     axios.get("http://localhost:8080/api/friend")
+    //         .then((response) => {
+    //             setFriends(response.data);
+    //         })
+    //         .catch((error) => {
+    //             console.error('데이터 불러오기 실패:', error);
+    //         });
+    // }, []);
 
     const defaultImg = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
     return (
@@ -21,18 +21,20 @@ const Friends = () => {
          <Header />
          <Body>
             <Title>친구 목록</Title>
+            <FriendsContainer>
             <FriendsList>
-                {friends.map((friend,index) => (
-            <FriendItem key={index}>
-            <Profile src={friend.profileImage || defaultImg} alt="Profile" />
+                {/* {friends.map((friend,index) => ( */}
+            <FriendItem>
+            <Profile src={defaultImg} alt="Profile" />
             <FriendDetails>
-                <FriendsItem>username: {friend.username}</FriendsItem>
-                <FriendsItem>MyTeam: {friend.myTeam}</FriendsItem>
+                <FriendsItem>username:</FriendsItem>
+                <FriendsItem>MyTeam:</FriendsItem>
             </FriendDetails>
             </FriendItem>
-             ))}
+             {/* ))} */}
             <ChatButton>채팅하기</ChatButton>
             </FriendsList>
+            </FriendsContainer>
             </Body>  
         </>
     );
@@ -47,23 +49,29 @@ height: 100%;
 `;
 
 const Title = styled.h1`
-margin:2rem;
+margin:3rem;
 font-weight:600;
 font-size:2.5rem;
+`;
+
+const FriendsContainer = styled.div`
+display:grid;
+grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+gap:2rem;
 `;
 
 const FriendsList = styled.ul`
 border:1px solid #ddd;
 list-style: none;
 width: 500px;
-margin:2rem;
-padding:1rem;
+margin-left:3rem;
+margin-bottom:2rem;
 background:white;
 border-radius: 8px;
 box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-display:grid;
-grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-gap:1.5rem;
+display:flex;
+justify-content:space-between;
+padding:0.6rem;
 `;
 
 const FriendItem = styled.li`
@@ -95,7 +103,6 @@ width: 60px;
 
 
 const ChatButton = styled.button`
-justify-self:end;
 width:100px;
 height:40px;
 background-color:gray;
@@ -105,5 +112,6 @@ border-radius:8px;
 font-size:1rem;
 cursor:pointer;
 box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+align-self:end;
 `;
 export default Friends;
